7b55ac66fc590846993e38a5940bf4aa344efd45
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-11-09 13:35:10 +0100

    add admin get all plans & user get created plans

--- routes/plan.ts
+++ routes/plan.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 
-import { addPlan, getPlan } from "../controllers/plans";
+import { addPlan, getPlan, updatePlan } from "../controllers/plans";
 
 router.route("/details").post(addPlan).get(getPlan);
+router.route("/update").patch(updatePlan);
 
 export default router;
